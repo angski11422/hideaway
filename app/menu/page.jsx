9957c7604dcc4a452238c80fb4a68ca66270005b
Angela Palaszewski <delarosa.ang@gmail.com>
2024-02-06 18:19:05 -0700

    main menu

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -3,22 +3,70 @@ export default function Menu() {
     <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-pink-300">
       <h1>Menu</h1>
       <div>
-        <h2>Appetizers</h2>
+        <h3 className="font-bold text-xl">Appetizers</h3>
+        <ul>
+          <li>Nacho Supreme</li>
+          <li>Bosco Cheese Sticks</li>
+          <li>Soft Pretzel</li>
+          <li>Homemade Chips</li>
+          <li>French Fries</li>
+          <li>Chili Cheese Fries</li>
+          <li>Onion Rings</li>
+          <li>Deep Fried Pickles</li>
+          <li>Fried Spicy Cauliflower</li>
+          <li>Deep Fried Mushrooms</li>
+          <li>Jalapeno Poppers</li>
+          <li>Cheese Curds</li>
+          <li>Mini Tacos</li>
+          <li>Southwestern Egg Rolls</li>
+          <li>Chicken Wings</li>
+        </ul>
       </div>
       <div>
-        <h2>Burgers & Dogs</h2>
+        <h3 className="font-bold text-xl">Burgers & Dogs</h3>
+        <ul>
+          <li>Angus Burger</li>
+          <li>Angus Cheeseburger</li>
+          <li>Mushroom Swiss Burger</li>
+          <li>Bacon Angus Cheeseburger</li>
+          <li>Hot Dog</li>
+          <li>2 Coney Dogs</li>
+        </ul>
       </div>
       <div>
-        <h2>Baskets</h2>
+        <h3 className="font-bold text-xl">Baskets</h3>
+        <ul>
+          <li>Fish Basket</li>
+          <li>Shrimp Basket</li>
+          <li>Chicken Strip Basket</li>
+        </ul>
       </div>
       <div>
-        <h2>Sandwiches</h2>
+        <h3 className="font-bold text-xl">Sandwiches</h3>
+        <ul>
+          <li>French Dip</li>
+          <li>Reuben</li>
+          <li>Patty Melt</li>
+          <li>Chicken Sandwich</li>
+          <li>BBQ Pulled Pork Sandwich</li>
+          <li>Grilled Cheese</li>
+          <li>Grilled Cheese & Ham</li>
+          <li>Grilled Bologna & Cheese</li>
+        </ul>
       </div>
       <div>
-        <h2>Soup & Salad</h2>
+        <h3 className="font-bold text-xl">Soup & Salad</h3>
+        <ul>
+          <li>Chicken Ceasar</li>
+          <li>Spicy Chicken</li>
+          <li>Chicken Bacon Ranch</li>
+          <li>Antipasto Salad</li>
+          <li>Chili</li>
+          <li>Soup of the Day</li>
+        </ul>
       </div>
       <div>
-        <h2>Pizza</h2>
+        <h3 className="font-bold text-xl">Pizza</h3>
       </div>
     </div>
   );
