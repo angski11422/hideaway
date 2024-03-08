9e36180c46a3cde689d9df3eec1109b7b1805d06
Angela Palaszewski <delarosa.ang@gmail.com>
2024-03-08 12:57:13 -0700

    import assets

--- app/components/styleguide.jsx
+++ app/components/styleguide.jsx
@@ -1,15 +1,26 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 const Styleguide = () => {
   return (
     <div>
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident
-        quaerat culpa magnam voluptatibus non dolorem aliquid error, esse
-        incidunt, iste quibusdam dolor tenetur mollitia natus odit a rerum!
-        Magnam, voluptates!
-      </p>
+      <div className="flex flex-col gap-y-4">
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident
+          quaerat culpa magnam voluptatibus non dolorem aliquid error, esse
+          incidunt, iste quibusdam dolor tenetur mollitia natus odit a rerum!
+          Magnam, voluptates!
+        </p>
+      </div>
+      <div className="flex flex-col gap-y-4">
+        <Link href="/">Link</Link>
+        <Button>Button 1</Button>
+        <Button>Button 2</Button>
+        <Button>Button 3</Button>
+      </div>
     </div>
   );
 };
